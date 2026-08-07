e924cdd40792c740d8b29df84e26e88b0b471c2a
Hsu <68013051+hsu-susan@users.noreply.github.com>
2026-08-07 14:59:16 +0900

    Enhance smoothie app with additional fruit search data
    Updated the data selection to include 'SEARCH_ON' and modified the nutrition information retrieval process to use the new column.

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,12 @@ st.write("The name on your Smoothie will be", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:"
@@ -30,9 +33,14 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        try:
+            search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+            st.subheader(fruit_chosen + ' Nutrition Information')
+            url = f"https://my.smoothiefroot.com/api/fruit/{search_on}"
+            response = requests.get(url, timeout=3)
+            response.raise_for_status()
+        except Exception:
+            st.info(f"Nutrition information for {fruit_chosen} is unavailable on trial accounts.")
 
     # st.write(ingredients_string)
 
